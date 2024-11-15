dee910e60d4b799be7f286d931c3ac713911abfb
connorGA <ronnoccap@gmail.com>
2024-11-15 10:46:56 -0800

    add feature : calendar, still need adjust styling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,33 @@ class HomeController < ApplicationController
 
   def index
     @user = current_user
-    @tasks = current_user.tasks || []
-    # @plots = current_user.plots if user_signed_in? || []
+    @tasks = user_signed_in? ? current_user.tasks : []
+    @months = generate_calendar
   end
+
+  private
+
+  def generate_calendar
+    start_of_year = Date.today.beginning_of_year
+    end_of_year = Date.today.end_of_year
+  
+    (start_of_year..end_of_year).group_by(&:month).map do |month, dates|
+      {
+        name: Date::MONTHNAMES[month],
+        days: dates.map do |date|
+          {
+            number: date.day,
+            class: if date == Date.today
+                     'current-day'
+                   elsif date < Date.today
+                     'past-day'
+                   else
+                     'future-day'
+                   end
+          }
+        end
+      }
+    end
+  end
+  
 end
--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,30 @@
 module TasksHelper
-end
+    def generate_calendar
+      current_date = Date.today
+      start_of_year = current_date.beginning_of_year
+      end_of_year = current_date.end_of_year
+  
+      calendar_html = ""
+  
+      (start_of_year..end_of_year).group_by(&:month).each do |month, days|
+        month_name = Date::MONTHNAMES[month]
+        calendar_html += "<div class='calendar-month #{month_name.downcase}'>"
+        calendar_html += "<h5>#{month_name}</h5>"
+        calendar_html += "<div class='small-calendar'>"
+  
+        days.each do |day|
+          classes = ["calendar-day"]
+          classes << "current-day" if day == current_date
+          classes << "past-day" if day < current_date
+          classes << "future-day" if day > current_date
+  
+          calendar_html += "<div class='#{classes.join(' ')}'>#{day.day}</div>"
+        end
+  
+        calendar_html += "</div></div>"
+      end
+  
+      calendar_html.html_safe
+    end
+  end
+  
