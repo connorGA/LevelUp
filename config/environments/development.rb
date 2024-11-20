be545c536855a30bb89bbb932bb09f99969dc42c
connorGA <ronnoccap@gmail.com>
2024-11-20 14:48:44 -0800

    fix caching issue, adjsut calendar styling and task page format

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,6 +71,13 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
+  #TO CLEAR CACHE DURING DEVELOPMENT
+  config.assets.debug = true
+  config.assets.digest = false
+  config.assets.compile = true
+  config.assets.quiet = true
+
+
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 end
